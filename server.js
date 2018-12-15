2a7202859aa4eaeec2b0551654ee2ddd14ee861f
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2018-12-15 02:41:14 +0100

    Updated.

--- server.js
+++ server.js
@@ -30,53 +30,110 @@ function httpHandler(req, res) {
             console.log("The file was saved!");
         });
     } else if (parsedUrl.href === '/read-json-file') {
-        const file = require('./States, Local Gorvenment Areas, Wards In Nigeria copy.js');
+        const file1 = require('./ward_and_local_governments_in_nigeria_with_country_statistics.js');
+        const file2 = require('./complete_polling_unit.js');
 
-        console.log('file', file);
-        const state = file.reduce((a,b) => {
-            let state = (b['STATE NAME'] !== '') ? {} : { name: b['STATE NAME'], code: b['STATE CODE'].toString(), lgas: []};
-            if (b['STATE NAME'] !== '') {
-                let lga = {};
-                let ward = {};
-                state.name = b['STATE NAME'];
-                state.code = b['STATE CODE'].toString();
-                state.lgas = [];
-                lga.name = b["LGA NAME"];
-                lga.code = b["LGA CODE"].toString();
-                lga.wards = [];
-                ward.name = b["WARD NAME"];
-                ward.code = b["WARD CODE"].toString();
-                lga.wards.push(ward);
-                state.lgas.push(lga);
-                a.push(state);
-            } else {
-                const lastState = a[a.length-1];
-                if (b['LGA NAME'] !== '') {
-                    let lga = {};
-                    let ward = {};
-                    lga.name = b["LGA NAME"];
-                    lga.code = b["LGA CODE"].toString();
-                    lga.wards = [];
-                    ward.name = b["WARD NAME"];
-                    ward.code = b["WARD CODE"].toString();
-                    lga.wards.push(ward);
-                    lastState.lgas.push(lga);
-                } else {
-                    let ward = {};
-                    ward.name = b["WARD NAME"];
-                    ward.code = b["WARD CODE"].toString();
-                    lastState.lgas[lastState.lgas.length-1].wards.push(ward);
+        console.log('file', file1);
+        console.log('file', file2);
+
+        for(let i = 0; i < file2.length; i++) {
+            const b = file2[i];
+            for(let j = 0; j < file1.states.length; j++) {
+                const a = file1.states[j];
+                // Get State
+                if (b['STATE NAME'] === a.name) {
+                    for(let k = 0; k < a.lgas.length; k++) {
+                        const c = a.lgas[k];
+                        // Get LGA
+                        if (b['LGA CODE'] === parseInt(c.code)) {
+                            for(let l = 0; l < c.wards.length; l++) {
+                                const d = c.wards[l];
+                                // Get Ward
+                                if (b['WARD CODE'] === parseInt(d.code)) {
+                                    let pollingUnit = {};
+                                    pollingUnit.name = b["POLLING STATION LOCATION/NAME"];
+                                    pollingUnit.code = b["POLLING STATION CODE"].toString();
+                                    pollingUnit.voters = [];
+                                    pollingUnit.GIS = {};
+                                    pollingUnit.noOfInecRegVoters = 0;
+                                    pollingUnit.noOfFdVoters = 0;
+                                    
+                                    if (d.pollingUnits) {
+                                        d.pollingUnits.push(pollingUnit);
+                                    } else {
+                                        d.pollingUnits = [];
+                                        d.pollingUnits.push(pollingUnit);
+                                    }
+                                    d.noOfPollingUnits ? d.noOfPollingUnits += 1: d.noOfPollingUnits = 1;
+                                    c.noOfPollingUnits ? c.noOfPollingUnits += 1: c.noOfPollingUnits = 1;
+                                    a.noOfPollingUnits ? a.noOfPollingUnits += 1: a.noOfPollingUnits = 1;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    break;
                 }
             }
-            
-            return a;
-        }, []);
+        }
 
-        console.log(state);
-        fs.appendFile("./data/ward_and_local_governments_in_Nigeria.json", JSON.stringify(state), (err) => {
+
+        console.log('States', file1);
+        // Get the total of all polling units to the country level
+        file1.noOfPollingUnits = file1.states.reduce((a,b) => a += b.noOfPollingUnits, 0);
+
+        console.log('Country', file1);
+
+        fs.appendFile("./data/complete_polling_unit_with_statistics.js", JSON.stringify(file1), (err) => {
             if(err) return console.log(err);
             console.log("The file was saved!");
         });
+        
+        // const state = file.reduce((a,b) => {
+        //     let state = (b['STATE NAME'] !== '') ? {} : { name: b['STATE NAME'], code: b['STATE CODE'].toString(), lgas: []};
+        //     if (b['STATE NAME'] !== '') {
+        //         let lga = {};
+        //         let ward = {};
+        //         state.name = b['STATE NAME'];
+        //         state.code = b['STATE CODE'].toString();
+        //         state.lgas = [];
+        //         lga.name = b["LGA NAME"];
+        //         lga.code = b["LGA CODE"].toString();
+        //         lga.wards = [];
+        //         ward.name = b["WARD NAME"];
+        //         ward.code = b["WARD CODE"].toString();
+        //         lga.wards.push(ward);
+        //         state.lgas.push(lga);
+        //         a.push(state);
+        //     } else {
+        //         const lastState = a[a.length-1];
+        //         if (b['LGA NAME'] !== '') {
+        //             let lga = {};
+        //             let ward = {};
+        //             lga.name = b["LGA NAME"];
+        //             lga.code = b["LGA CODE"].toString();
+        //             lga.wards = [];
+        //             ward.name = b["WARD NAME"];
+        //             ward.code = b["WARD CODE"].toString();
+        //             lga.wards.push(ward);
+        //             lastState.lgas.push(lga);
+        //         } else {
+        //             let ward = {};
+        //             ward.name = b["WARD NAME"];
+        //             ward.code = b["WARD CODE"].toString();
+        //             lastState.lgas[lastState.lgas.length-1].wards.push(ward);
+        //         }
+        //     }
+            
+        //     return a;
+        // }, []);
+
+        // console.log(state);
+        // fs.appendFile("./data/ward_and_local_governments_in_Nigeria.json", JSON.stringify(state), (err) => {
+        //     if(err) return console.log(err);
+        //     console.log("The file was saved!");
+        // });
     } else if (parsedUrl.href === '/add-extra-data') {
         const file = require('./ward_and_local_governments_in_Nigeria_new.js');
 
